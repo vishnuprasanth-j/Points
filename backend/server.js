86ee8f8503158837269a1024d9edd5d294646664
vishnuprasanth-j <61909168+vishnuprasanth-j@users.noreply.github.com>
2022-09-06 17:54:30 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,16 @@ const auth =require('./Middleware.js')
 const app= express()
 
 app.use(express.json())
-app.use(cors())
+
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ 
+app.use(cors(corsOptions)) 
+
+
 
 dotenv.config()
 
